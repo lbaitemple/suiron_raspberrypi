c297acc60127779d1f11a5c38c51f7a7589fa4ed
Jay Vongphachanh <jayvong136@gmail.com>
2019-07-15 14:25:03 -0400

    Add files via upload

--- newverison/catkin_ws/src/simp_motor/src/Joydrive22Revision3.py
+++ newverison/catkin_ws/src/simp_motor/src/Joydrive22Revision3.py
@@ -13,7 +13,7 @@ import json
 import time
 from cv_bridge import CvBridge, CvBridgeError
 import Adafruit_PCA9685
-from threading import Lock
+
 #-----------------------------------------------------------------
 REV = 2.3 # select your board 
 # If your board has a PWM module built in we will need the correct chip
@@ -27,7 +27,7 @@ if REV == 2.3:
 maxspeed=0.17
 minspeed=0.13
 #-------------------------------------------------------------
-#max_line=rospy.get_param('~max_line', 100)
+
 #Our attempt to speed up the process
 nos = os.system
 #buttonWasPressed = False
@@ -35,13 +35,12 @@ nos = os.system
 with open('/home/ubuntu/settings.json') as d:
     SETTINGS = json.load(d)
 br=CvBridge()
-count = 0
+
 # Instantiatees our IO class
 print(SETTINGS['width'], SETTINGS['height'])
 suironio = SuironIO(id=0, width=SETTINGS['width'], height=SETTINGS['height'], depth=SETTINGS['depth'])
 suironio.init_saving()
-lock = Lock()
-clck=Clock(suironio, 0.01)
+clck=Clock(suironio, 10)
 clck.start()
 
 if REV == 2.2:
@@ -64,68 +63,39 @@ def setspeed22(pin, sped):
 
 def setspeed23(pin, sped):
     pos = int(sped*4096)
-    print(pos)
+   # print(pos)
     pwm.set_pwm(pin, 0, pos)
 
 def callback(data):
-    global count
-#    global max_line
-#    global buttonWasPressed
-#    if (not buttonWasPressed and
-#           ( data.axes[0] != 0 or 
-#           data.axes[1] != 0)):
-#    print(data.angular.z, data.linear.x)
-    print("locking", max_line)
-    
-#    print "Delay:%6.3f" % (rospy.Time.now() - data.header.stamp).to_sec()
-    lock.acquire()
-    if (not suironio.check_lock()):
-        turn = abs((data.angular.z/8)-0.15)
-        speed  = (data.linear.x/14)+0.15    
-#    if(speed != drive_msg): 
-#       turn = abs((data.angular.z/8)-0.15)
-#       speed  = (data.linear.x/14)+0.15    
-#        turn = abs(0.05*data.axes[0]-0.15)
-#        speed  = 0.05*data.axes[1]+0.15     
-  # pwm.set_pwm(8, 0, int(speed))
-   # pwm.set_pwm(9, 0, int(turn))
-        s={}
-#        s['motor']=speed
-#        s['servo']=turn
-#        suironio.record_inputs(s)
-        if speed > maxspeed:
-            speed = maxspeed
-        elif speed < minspeed:   
-            speed = minspeed
-#    print("locking")
-#    lock.acquire()
-        if REV == 2.2:
+   # global buttonWasPressed
+   # if (not buttonWasPressed and
+   #        ( data.axes[0] != 0 or 
+   #        data.axes[1] != 0)):
+
+    turn = (data.angular.z/8)+0.15
+    speed  = (data.linear.x/14)+0.15    
+    pwm.set_pwm(8, 0, int(speed))
+    s={}
+    s['motor']=speed
+    s['servo']=turn
+#    suironio.record_inputs(s)
+    if speed > maxspeed:
+        speed = maxspeed
+    elif speed < minspeed:   
+        speed = minspeed
+    if REV == 2.2:
            # print("This is Motor values" , speed)
-            setspeed22(12, speed)
+        setspeed22(12, speed)
        # print("This is servo values" , turn)
-            setspeed22(13, turn)
-        elif REV == 2.3:
+        setspeed22(13, turn)
+    elif REV == 2.3:
        # print("This is Motor values for 2.3" , speed)
-            setspeed23(8, speed)
+        setspeed23(8, speed)
        # print("This is Motor values for 2.3" , turn)
-            setspeed23(9,turn)
+        setspeed23(9,turn)
    # buttonWasPressed = True
-        s['motor']=speed
-        s['servo']=turn
-        print("recording your stuff")
-        if count < max_line:
-            suironio.record_inputs(s)
-            suironio.unlock()
-            count = count + 1
-            lock.release()
-            print("Releasing")
-        else:
-            count = 0
-            print("Releasing")
-            suironio.reinit_saving()
-            suironio.record_inputs(s)
-            suironio.unlock()
-            lock.release()
+    suironio.record_inputs(s)
+
 #START
 REV == 2.3
 if __name__ == '__main__':
@@ -134,21 +104,13 @@ if __name__ == '__main__':
             nos(cmd[i])
             print(cmd[i])
     global buttonWasPressed
-    global max_line
-    max_line=rospy.get_param('max_line', 200)
-    buttonWasPressed = False
-#    print("locking")
-#    lock.acquire()
-    rospy.init_node('sss')
-    sub=rospy.Subscriber("cmd_vel", Twist, callback, queue_size=1, tcp_nodelay=True, buff_size=2**24)
-#    sub=rospy.Subscriber("joy", Joy, callback)
-#    lock.release()
-#    print("Releasing") 
-    pub=rospy.Publisher('carimage/raw_image/compressed', Image, queue_size = 1, tcp_nodelay=True) 
-    rate = rospy.Rate(15)
-#    lock.release()
-#    print("Releasing")   
     
+    buttonWasPressed = False
+    rospy.init_node('sss')
+    sub=rospy.Subscriber("teleop_twist_joy", Twist, callback)
+    pub=rospy.Publisher('carimage/raw_image/compressed', Image, queue_size = 1) 
+    rate = rospy.Rate(10)
+
     while  not rospy.is_shutdown():
         try:
             img=suironio.get_camframe()
